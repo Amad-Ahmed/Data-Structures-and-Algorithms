f7b872627d0ba43f95be847b4e3329939baaa315
Amad Siddiqui <80278397+Amad-Ahmed@users.noreply.github.com>
2023-03-10 22:11:58 +0500

    tree solving and roadmap

---
+++ Leetcode questions/leetcode_1379.py
@@ -0,0 +1,15 @@
+# stack = [(original, cloned)]
+
+#         while stack:
+#             node1, node2 = stack.pop()
+
+#             if node1 == target:
+#                 return node2
+
+#             if node1.left:
+#                 stack.append((node1.left, node2.left))
+
+#             if node1.right:
+#                 stack.append((node1.right, node2.right))
+
+#         return None
